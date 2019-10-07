8033f3908c4a6c341f300b8df2223e0db3acc4df
Barath99 <exploretechsbk@gmail.com>
2019-10-07 19:23:09 +0530

    Login page Styling is done

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.getElementById("chk-btn1").style.borderBottomColor = "#064acb";
+document.getElementById("register-form").style.display = "none";
+
+	  function func_loginBtn() {
+	    document.getElementById("login-form").style.display = "block";
+	    document.getElementById("register-form").style.display = "none";
+	    document.getElementById("chk-btn1").style.borderBottomColor = "#064acb";
+	    document.getElementById("chk-btn2").style.borderBottomColor = "transparent";
+
+	  }
+
+	  function func_registerBtn() {
+	    document.getElementById("login-form").style.display = "none";
+	    document.getElementById("register-form").style.display = "block";
+	    document.getElementById("chk-btn1").style.borderBottomColor = "transparent";
+	    document.getElementById("chk-btn2").style.borderBottomColor = "#064acb";
+	  }
